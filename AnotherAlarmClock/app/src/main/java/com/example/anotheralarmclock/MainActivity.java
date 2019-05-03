fb66e79769b291a877a5d2a988124483284dd533
Sruthi Jaganathan <sjaganat@ucsc.edu>
2019-05-02 18:33:36 -0700

    Added time pickers, and some more UI modifications

--- AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/MainActivity.java
+++ AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.anotheralarmclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button startTime = (Button) findViewById(R.id.startButton);
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startNewActivity = new Intent(MainActivity.this, startButton.class);
+                startActivity(startNewActivity);
+            }
+        });
+
+        final Button endTime = (Button) findViewById(R.id.EndButton);
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startNewActivity = new Intent(MainActivity.this, endButton.class);
+                startActivity(startNewActivity);
+            }
+        });
     }
 }
