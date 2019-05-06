6e7e4bfff04985bcf601decbeef64576966b6550
katkell <katkellx@gmail.com>
2019-05-06 16:52:06 -0700

    linked start and end times to main, added constraints

--- AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/endButton.java
+++ AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/endButton.java
@@ -1,7 +1,9 @@
 package com.example.anotheralarmclock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class endButton extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class endButton extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_button);
     }
+
+    public void backToStart(View view){
+        Intent intent = new Intent(endButton.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/startButton.java
+++ AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/startButton.java
@@ -1,7 +1,9 @@
 package com.example.anotheralarmclock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class startButton extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class startButton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_button);
+    }
+
+    public void backToStart(View view){
+        Intent intent = new Intent(startButton.this, MainActivity.class);
+        startActivity(intent);
     }
 }
