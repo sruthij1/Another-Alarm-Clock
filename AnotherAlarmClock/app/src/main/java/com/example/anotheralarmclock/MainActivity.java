41c23b22cca1eadd3a0725c305e4609a9a4209f8
Sruthi Jaganathan <sjaganat@ucsc.edu>
2019-05-23 00:45:45 -0700

    Made alarm ring when start time of set alarm is reached; problem:keeps ringing once it starts

--- AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/MainActivity.java
+++ AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.anotheralarmclock;
 
 import android.app.TimePickerDialog;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextClock;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText chooseStartTime, chooseEndTime;
+    TextClock nowTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //nowTime from the TextClock
+        nowTime = findViewById(R.id.nowTime);
 
         // Starting new time picker code
         chooseStartTime = findViewById(R.id.etChooseTime);
@@ -28,16 +38,45 @@ public class MainActivity extends AppCompatActivity {
                 Calendar calendar = Calendar.getInstance();
                 int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
                 int currentMinute = calendar.get(Calendar.MINUTE);
+                Log.d(Integer.toString(currentHour), "startTime");
                 TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        String start = "";
                         String amPm;
-                        if (hourOfDay >= 12) {
-                            amPm = "PM";
+                        String min =Integer.toString(minutes);
+
+                        if (hourOfDay > 12) {
+                            hourOfDay = hourOfDay - 12;
+                            amPm = " PM";
                         } else {
-                            amPm = "AM";
+                            amPm = " AM";
                         }
-                        chooseStartTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+
+                        if (Integer.valueOf(min) < 10){
+                            start = Integer.toString(hourOfDay).concat(":").concat("0" + min).concat(amPm);
+                        } else {
+                            start = Integer.toString(hourOfDay).concat(":").concat(min).concat(amPm);
+                        }
+
+                        //start = String.format("%02d:%02d", hourOfDay, minutes) + amPm;
+                        chooseStartTime.setText(start);
+                        final boolean check = nowTime.getText().toString().equals(start);
+
+                        final Ringtone sound = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+
+                        Timer t = new Timer();
+                        t.scheduleAtFixedRate(new TimerTask() {
+
+                            public void run(){
+                                if (check == true){
+                                    sound.play();
+                                } else{
+                                    sound.stop();
+                                }
+                            }
+
+                        } ,0, 1000);
                     }
                 },  currentHour, currentMinute, false);
                 timePickerDialog.show();
--- AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/startButton.java
+++ AnotherAlarmClock/app/src/main/java/com/example/anotheralarmclock/startButton.java
@@ -1,17 +1,75 @@
 package com.example.anotheralarmclock;
 
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextClock;
+import android.widget.TimePicker;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class startButton extends AppCompatActivity {
+
+    TimePicker alarmTime;
+    TextClock currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_button);
+        alarmTime = findViewById(R.id.timePicker);
+        currentTime = findViewById(R.id.textClock0);
+        final Ringtone sound = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            public void run(){
+                if (currentTime.getText().toString().equals(AlarmTime())){
+                    sound.play();
+                } else{
+                    sound.stop();
+                }
+
+            }
+
+        } ,0, 1000);
     }
+
+    public String AlarmTime(){
+        String time;
+        String minutes = "";
+        Integer hour = alarmTime.getHour();
+        Integer min = alarmTime.getMinute();
+
+        Log.d(hour.toString(), "hour: ");
+        Log.d(min.toString(), "min: ");
+
+        if (10>min){
+            minutes.concat("0").concat(min.toString());
+        } else {
+            minutes.concat(min.toString());
+        }
+
+        if (hour >12){
+            Log.d(hour.toString(), "heyy1: ");
+            hour = hour-12;
+            time = hour.toString().concat(":").concat(minutes).concat(" PM");
+        } else {
+            Log.d(hour.toString(), "heyy: ");
+            time = hour.toString().concat(":").concat(minutes).concat(" AM");
+        }
+
+        Log.d(time, "time: ");
+
+        return time;
+    }
+
 
     public void backToStart(View view){
         Intent intent = new Intent(startButton.this, MainActivity.class);
